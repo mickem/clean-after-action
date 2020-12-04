9294aa1d1273a024d2433c6a9869633d35a89227
Michael Medin <michael.medin@pricer.com>
2020-12-04 07:40:40 +0100

    Added keepgit flag

--- cleanup.js
+++ cleanup.js
@@ -2,13 +2,21 @@ const core = require('@actions/core');
 const io = require('@actions/io');
 const fs = require('fs');
 
+function getBoolValue(name) {
+  return ['false', '0', '', 'no', 'n'].includes(core.getInput(name).trim().toLowerCase())
+}
+
 async function main() {
   try {
+    const keepGit = getBoolValue('keepGit');
     await fs.readdir('.', async (err, files) => {
       if (err) {
         throw new Error(`Failed to list files: ${err}`);
       }
       for (const file of files) {
+        if (keepGit && file === '.git') {
+          continue;
+        }
         console.log(`Deleting ${file}`);
         await io.rmRF(file);
       }
